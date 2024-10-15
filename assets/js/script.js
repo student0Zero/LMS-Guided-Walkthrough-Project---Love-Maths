e502fd5026cdf8185363fec2e6fc349d7fa5dc4b
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-10-15 09:36:38 +0000

    Add display addition questions to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,10 +12,12 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('You clicked Submit!');
       } else {
         let gameType = this.getAttribute('data-type');
-        alert(`You clicked ${gameType}`);
+        runGame(gameType);
       }
     });
   }
+
+  runGame('addition');
 });
 
 // FUNCTIONS
@@ -23,9 +25,17 @@ document.addEventListener('DOMContentLoaded', function () {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
+  // create two random numbers between 1 and 25
   let num1 = Math.floor(Math.random() * 25) + 1;
   let num2 = Math.floor(Math.random() * 25) + 1;
+
+  if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}, Aborting`;
+  }
 }
 
 function checkAnswer() {}
@@ -36,7 +46,11 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('opreator').textContent = '+';
+}
 
 function displaySubtractQuestion() {}
 
