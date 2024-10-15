a640a17d29e51b243b39d21f75ac4631aec69be1
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-10-15 14:14:31 +0000

    add division game play

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,6 +47,8 @@ function runGame(gameType) {
 
   if (gameType === 'addition') {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === 'division') {
+    displayDivisionQuestion(num1, num2);
   } else if (gameType === 'multiply') {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === 'subtract') {
@@ -90,6 +92,8 @@ function calculateCorrectAnswer() {
 
   if (operator === '+') {
     return [operand1 + operand2, 'addition'];
+  } else if (operator === '/') {
+    return [operand1 / operand2, 'division'];
   } else if (operator === '-') {
     return [operand1 - operand2, 'subtract'];
   } else if (operator === 'x') {
@@ -135,4 +139,11 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = 'x';
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+  operand1 = operand1 * operand2;
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = '/';
 }
