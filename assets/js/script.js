5d44b0cc008257ff4911bf6ed0ac2a3a803c22ab
Ash Hawrani <dkwqhej8@students.codeinstitute.net>
2024-10-15 07:40:05 +0000

    Add game js functions structure

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function runGame() {}
+
+function checkAnswer() {}
+
+function CalculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
